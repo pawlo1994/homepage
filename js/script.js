d5f194f01f07043bf97d2f250b7e2cbb94d7f896
pawlo1994 <pawlo4-94@o2.pl>
2021-04-28 14:40:26 +0200

    functions edited

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 {
-    const changeThemeButton = document.querySelector(".js-changeThemeButton");
-    const imageButton = document.querySelector(".js-imageButton");
-
-    const init = () => {
+    const welcome = () => {
         console.log("welcome to my personal homepage");
-    }
+    };
 
     const changeTheme = () => {
         const container = document.querySelector(".js-container");
@@ -13,20 +10,21 @@
         changeThemeButtonInner.innerText = changeThemeButtonInner.innerText === "Ciemne" ? "Jasne" : "Ciemne";
     };
 
-    const hideImage = () => {
+    const toggleImage = () => {
         const sectionImage = document.querySelector(".js-sectionImage");
         const imageButtonInner = document.querySelector(".js-imageButtonInner");
         sectionImage.classList.toggle("section__image--hidden");
         imageButtonInner.innerText = imageButtonInner.innerText === "Ukryj" ? "Pokaż" : "Ukryj";
     };
 
+    const init = () => {
+        const changeThemeButton = document.querySelector(".js-changeThemeButton");
+        const imageButton = document.querySelector(".js-imageButton");
+        changeThemeButton.addEventListener("click", changeTheme);
+        imageButton.addEventListener("click", toggleImage);
+    };
+
+    welcome();
     init();
 
-    changeThemeButton.addEventListener("click", () => {
-        changeTheme();
-    });
-
-    imageButton.addEventListener("click", () => {
-        hideImage();
-    });
 }
